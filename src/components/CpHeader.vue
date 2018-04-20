91ac337e752bf30baceddea51898f600ba578810
JW-KIM <jw4005.kim@gmail.com>
2018-04-21 01:13:14 +0900

    화면작업

--- src/components/CpHeader.vue
+++ src/components/CpHeader.vue
@@ -2,42 +2,7 @@
     <header id="header" class="container">
         <div class="wrapper">
             <div class="cell cell-left textleft">
-                <div id="menu">
-                    <nav>
-                        <ul>
-                            <li id="menu-item-1308"
-                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-10 current_page_item menu-item-1308">
-                                <a href="http://liviucerchez.com/cross/">Home</a></li>
-                            <li id="menu-item-1310"
-                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1310"><a
-                                    href="http://liviucerchez.com/cross/portfolio/">Portfolio</a></li>
-                            <li id="menu-item-1309"
-                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1309"><a
-                                    href="http://liviucerchez.com/cross/blog/">Blog</a></li>
-                            <li id="menu-item-1317"
-                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1317"><a
-                                    href="http://liviucerchez.com/cross/shop/">Shop</a></li>
-                            <li id="menu-item-1307"
-                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-1307">
-                                <a href="http://liviucerchez.com/cross/features/">Features</a>
-                                <ul class="sub-menu">
-                                    <li id="menu-item-1319"
-                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1319">
-                                        <a
-                                                href="http://liviucerchez.com/cross/503-maintenance/">503 –
-                                            Maintenance</a></li>
-                                    <li id="menu-item-1318"
-                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1318">
-                                        <a
-                                                href="http://liviucerchez.com/cross/sidebar-page/">Sidebar Page</a></li>
-                                </ul>
-                            </li>
-                            <li id="menu-item-1306"
-                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1306"><a
-                                    href="http://liviucerchez.com/cross/contact/">Contact</a></li>
-                        </ul>
-                    </nav>
-                    <span id="menu-toggle"><em></em></span></div>
+                <cp-header-menu></cp-header-menu>
             </div>
             <div class="cell cell-middle textcenter">
                 <div id="logo"><h1><a href="http://liviucerchez.com/cross/" rel="home"><img
@@ -54,7 +19,10 @@
 </template>
 
 <script>
+    import CpHeaderMenu from "./CpHeaderMenu.vue";
+
     export default {
+        components: {CpHeaderMenu},
         name: "cp-header"
     }
 </script>
---
+++ src/components/CpHeaderMenu.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="menu" v-bind:class="{collapse : menuOpenYn}"
+         @mouseover="openMenu"
+         @mouseleave="closeMenu"
+    >
+        <nav>
+            <ul @click="closeMenu">
+                <li id="menu-item-1308"
+                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-10 current_page_item menu-item-1308">
+                    <router-link to="/home">Home</router-link>
+                </li>
+                <li id="menu-item-1310"
+                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1310">
+                    <router-link to="/portfolio">Portfolio</router-link>
+                </li>
+                <li id="menu-item-1309"
+                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1309">
+                    <router-link to="/shop">Shop</router-link>
+                </li>
+                <li id="menu-item-1317"
+                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1317">
+                    <router-link to="/contact">Contact</router-link>
+                </li>
+            </ul>
+        </nav>
+        <span id="menu-toggle"
+              v-on:click="toogleMenu"
+        ><em></em></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "cp-header-menu",
+        data() {
+            return {
+                menuOpenYn: false
+            }
+        },
+        methods: {
+            toogleMenu() {
+                this.menuOpenYn = !this.menuOpenYn;
+            },
+            openMenu() {
+                this.menuOpenYn = true;
+            },
+            closeMenu() {
+                this.menuOpenYn = false;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

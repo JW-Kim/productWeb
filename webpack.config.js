1fc5315b3f44055ba73bfa5d452acd69b29d56e1
JWKIM <jw4005.kim@gmail.com>
2019-10-28 19:38:29 +0900

    setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,65 @@
-var webpack = require('webpack')
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: [
-    './src/main.js'
-  ],
-  output: {
-    path: "/dist/js",
-    publicPath: "/dist/",
-    filename: "app.js"
-  },
-  watch: true,
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        // excluding some local linked packages.
-        // for normal use cases only node_modules is needed.
-        exclude: /node_modules|vue\/src|vue-router\//,
-        loader: 'babel'
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue'
-      }
-    ]
-  },
-  babel: {
-    presets: ['es2015'],
-    plugins: ['transform-runtime']
-  },
-  resolve: {
-    modulesDirectories: ['node_modules'],
-    alias: {
-      'vue$': 'vue/dist/vue.common.js'
+    devtool: 'eval-source-map',
+    entry: [
+        'babel-polyfill',
+        'webpack-dev-server/client?http://localhost:9000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        path.join(__dirname, 'src/index.js')
+    ],
+    output: {
+        path: path.join(__dirname, '/dist/'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: 'src/index.tpl.html',
+          inject: 'body',
+          filename: 'index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+    eslint: {
+        configFile: '.eslintrc',
+        failOnWarning: false,
+        failOnError: false
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint'
+            }
+        ],
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            {
+                test: /\.json?$/,
+                loader: 'json'
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'
+            },
+            { test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+            { test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file' }
+        ]
     }
-  }
-}
+};
